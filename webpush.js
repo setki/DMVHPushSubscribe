4be2f3a477b9113c1aa0795a79e3ce255b838fe8
DMVH\st <st@dmvh.eu>
2022-02-04 17:24:03 +0100

    fix

--- webpush.js
+++ webpush.js
@@ -26,8 +26,3 @@ const pushSubscription = {
 // The actual payload to push
 webpush.sendNotification(pushSubscription, 'ST Push Info');
 
-// Server Express
-// ----------------------------------------------------------------------------
-const app = express();
-const port = 5000;
-app.listen(port, () => console.log(`Server started on port ${port}`));
