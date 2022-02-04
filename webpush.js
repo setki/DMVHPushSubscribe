e3e1c13a43eed89eb27e53a98b57f28ad503d2e2
DMVH\st <st@dmvh.eu>
2022-02-04 17:50:55 +0100

    add subscribe data

--- webpush.js
+++ webpush.js
@@ -13,15 +13,18 @@ const vapidKeys = {
 webpush.setVapidDetails('mailto:st@dmvh.eu', vapidKeys.publicKey, vapidKeys.privateKey);
 
 // This is the same output of calling JSON.stringify on a PushSubscription
-// It is the data of push in index.html that returns to subscribe
-// Should be pulled from database. Here hardcoded to simulate the dataretrieve
-const pushSubscription = {
-  endpoint: '.....',
-  keys: {
-    auth: '.....',
-    p256dh: '.....'
-  }
+// It is the data of push in index.html that returns the subscribe confirmation.
+// Should be stored in database and repulled for the push. But here hardcoded to simulate it
+const pushSubscription = 
+{
+    "endpoint": "https://fcm.googleapis.com/fcm/send/cIgytaNg-Kg:APA91bF9JQR5OtBV_8NEj9XNqZLxyxd2k43VhPQ1RBzf4oNcTulHUg2J5BT3ep7FYZH8dWQSEma9mYhsQVlVKhpemuWcIlyecK7v4B5Fh_ir3bXZXwpBg_E8vJVDwFBme6U-eMTQcQ2_",
+    "expirationTime": null,
+    "keys": {
+        "p256dh": "BNyqPYGibH9jqE5w254Y_0DNN7p8_Gdn1AbsvOiLTfJwUeUIHndOge3-vU4h6-1o5QgRdqDvqjjzysk13gaAvbA",
+        "auth": "r1jmzmAoECx8vW7-wRkwMA"
+    }
 };
+
 
 // The actual payload to push
 webpush.sendNotification(pushSubscription, 'ST Push Info');
