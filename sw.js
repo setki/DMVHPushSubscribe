d694a206c2f0d7ab848d265be6414e6902144a54
DMVH\st <st@dmvh.eu>
2022-02-07 14:10:44 +0100

    fixing push nex WS

--- sw.js
+++ sw.js
@@ -3,47 +3,32 @@
 /* See : https://developers.google.com/web/ilt/pwa/introduction-to-push-notifications   */
 /* ------------------------------------------------------------------------------------ */
 
-self.addEventListener('push', function (e) {
+self.addEventListener('push', function(e) {
+
+    if (e.data) {
+        bodytxt = e.data.text();
+    } else {
+        bodytxt = 'You are subscribed to DMVH newsletter.'; // Push message no payload;
+    }
+
     var options = {
-        body: 'You are subscribed to DMVH newsletter.',
-        icon: 'images//dmvh_ico_blue.png',
+        body: bodytxt,
+        icon: 'images/dmvh_ico_blue.png',
         vibrate: [100, 50, 100],
         data: {
             dateOfArrival: Date.now(),
-            primaryKey: '2'
+            primaryKey: 1
         },
         actions: [
-            {
-                action: 'explore', title: 'See Archive'
-                /*icon: 'images/subscribe_128.png'*/
+            { action: 'explore', title: 'See Archive',
+                icon: 'images/subscribe_128.png'
             },
-            {
-                action: 'close', title: 'Close'
-                /*icon: 'images/ignore_128.png'*/
+            { action: 'close', title: 'Close',
+                icon: 'images/ignore_128.png'
             },
         ]
     };
     e.waitUntil(
-        self.registration.showNotification('DMVH Newsletter', options)
+        self.registration.showNotification('DMVH Newsletter - ' + primaryKey, options)
     );
 });
-
-//self.addEventListener('notificationclose', function (e) {
-//    var notification = e.notification;
-//    var primaryKey = notification.data.primaryKey;
-
-//    console.log('Closed notification: ' + primaryKey);
-//});
-
-//self.addEventListener('notificationclick', function (e) {
-//    var notification = e.notification;
-//    var primaryKey = notification.data.primaryKey;
-//    var action = e.action;
-
-//    if (action === 'close') {
-//        notification.close();
-//    } else {
-//        clients.openWindow('http://www.dmvh.eu');
-//        notification.close();
-//    }
-//});
--- webpush.js
+++ webpush.js
@@ -1,47 +1,55 @@
 // 
 // ----------------------------------------------------------------------------
 
-const webpush = require('web-push');
+const webpush = require("web-push");
 
 // VAPID keys should only be generated only once.
 const vapidKeys = {
-    publicKey:  'BAVIUVK1GtRFMFlbgd4vfHCnPrXIDiZmeSPohaAjHw2mE97d2X_kzLmFd_CLG7Ol5RAKP4a3MSaDhljLk1YX0F0',
-    privateKey: 'ZpljSDgi6zbrOf2y5rgn3BpSXIDC5uuY0YY0ilhij_k'
+    publicKey:  "BAVIUVK1GtRFMFlbgd4vfHCnPrXIDiZmeSPohaAjHw2mE97d2X_kzLmFd_CLG7Ol5RAKP4a3MSaDhljLk1YX0F0",
+    privateKey: "ZpljSDgi6zbrOf2y5rgn3BpSXIDC5uuY0YY0ilhij_k"
 };
 
 // Set Push Notification Security
-webpush.setVapidDetails('mailto:st@dmvh.eu', vapidKeys.publicKey, vapidKeys.privateKey);
+webpush.setVapidDetails("mailto:st@dmvh.eu", vapidKeys.publicKey, vapidKeys.privateKey);
 
 // This is the same output of calling JSON.stringify on a PushSubscription
 // It is the data of push in index.html that returns the subscribe confirmation.
 // Should be stored in database and repulled for the push. But here hardcoded to simulate it
+
+// Edge ST  (OK)
+//var uri     = "https://wns2-db5p.notify.windows.com/w/?token=BQYAAACFrSzZWfLQI3wNO94SF0VN89ILfve7VDDJ9WXT3f7%2fA113%2f5V%2fxTvK2SDn%2b3luqv8XeIAiBwB1ge9K6ytnrYO0cQyzzAlYH1XFe%2bpLselLgtpCM8U0DYXDVMxKmlQGRf51Yur5jVexpuHnl0jkcyAa1%2fPc58SSR2RU%2bKUXHrCAUCZtvZ0W7cbZ5jbLuxxibaMR2ES5ZBDz02ZcMeUPhkTBCgNyY7u9xtEmHXfUGRQtX3rpcAKCCpYeEGp6m1Q91QXNOTI%2bx9C7SFnCubHPsUgozKlVNzjmKJ4XCFWtqq4oUcHH5sE6x696I44P1FdYoGWeWsrwPqT62tR4er7xbcVt";
+//var p256dh  = "BL8DqMxQ4dHM6X9B71iU4tcGV7ArTW0gVneMTqKXpzbGTCHpXGKEvLShCtLCyH7Ur14N60wf--tQGeoXLvFo4WE";
+//var auth    = "4YHRcd5BBz8fRqasroCszw";
+
+// Explorer ST
+//var uri    = "https://fcm.googleapis.com/fcm/send/eL_-P1wKezU:APA91bHjnWdEnMMymS8FNnfqohpnA8QkD0SDqHeOf1elpX1Hd0TcZo7qE7bZ7fIvbFSG-82YFT5gTH3tHCZ4rB-N2cQNgGNGG_9IzMLT5Q3pL0UA5BZZL4yFf9NRTl7LBONF1fWDP5uW";
+//var p256dh = "BIOkNM2pgr3 - XzcEGVd4_ - nK - m7D2FvkQt3aGJqrQVQLMM7LafSxF0 - tcs3WDINidxno35z2xTwUN46hvl1ikJk";
+//var auth   = "-zrXw0ZMRFpY1zm8QBL4HA";
+
+// Explorer CP
+//var uri    = "https://fcm.googleapis.com/fcm/send/f_tFGwZ9lBY:APA91bHnsl-ML0mko8v8UCvY1DrIsndn_Ztamc7BUvAD8oScyF1zR6xmyZD6quu_-PLSX8bJxqFoIw6XStEE_QB1KscoYw4wTp_qglzvmwNEpZ0XrTo91DxANQHg1IvmD3WjooCc153x",
+//var p256dh = "BJcR3HMUYiQoIOMJbIuvW7hO8ReprkOa_I1rjqOjj5Jta3UViPU5bixMzoaxlVxcpjFwXciqVgrBKMtuddMh2Lk";
+//var auth   = "7gy4jP_uXxVFuANl-SREMw";
+
+// Mozilla ST
+var uri    = "https://updates.push.services.mozilla.com/wpush/v2/gAAAAABh_…4KMkvkwYq005NVsq15rg4PSvLdKl3r6VV6oupdHwrWJB8bFN7fl_UjM3BX18";
+var p256dh = "BDw3j-CUVMZXsH3SLZhDHVXEXQC4h2SZ_iEwcY95g-dUik3-0s22QmaJQf3GfDDdVIMFqROWZEMIc5F4bjk0F8M";
+var auth   = "K7rRDSumFSKN7bGaj345TA";
+//var res = encodeURI(uri);
+
+
 const pushSubscription = 
 {
-
-    //"endpoint": "https://fcm.googleapis.com/fcm/send/eL_-P1wKezU:APA91bHjnWdEnMMymS8FNnfqohpnA8QkD0SDqHeOf1elpX1Hd0TcZo7qE7bZ7fIvbFSG-82YFT5gTH3tHCZ4rB-N2cQNgGNGG_9IzMLT5Q3pL0UA5BZZL4yFf9NRTl7LBONF1fWDP5uW",
-    //"expirationTime": null,
-    //"keys": {
-    //    "p256dh": "BIOkNM2pgr3-XzcEGVd4_-nK-m7D2FvkQt3aGJqrQVQLMM7LafSxF0-tcs3WDINidxno35z2xTwUN46hvl1ikJk",
-    //    "auth": "-zrXw0ZMRFpY1zm8QBL4HA"
-    //}
-
-    "endpoint": "https://updates.push.services.mozilla.com/wpush/v2/gAAAAABh_…4KMkvkwYq005NVsq15rg4PSvLdKl3r6VV6oupdHwrWJB8bFN7fl_UjM3BX18",
+    "endpoint": uri,
     "expirationTime": null,
     "keys": {
-        "auth": "K7rRDSumFSKN7bGaj345TA",
-        "p256dh": "BDw3j-CUVMZXsH3SLZhDHVXEXQC4h2SZ_iEwcY95g-dUik3-0s22QmaJQf3GfDDdVIMFqROWZEMIc5F4bjk0F8M"
+        "p256dh": p256dh,
+        "auth": auth
     }
-
-    //{
-    //"endpoint": "https://fcm.googleapis.com/fcm/send/f_tFGwZ9lBY:APA91bHnsl-ML0mko8v8UCvY1DrIsndn_Ztamc7BUvAD8oScyF1zR6xmyZD6quu_-PLSX8bJxqFoIw6XStEE_QB1KscoYw4wTp_qglzvmwNEpZ0XrTo91DxANQHg1IvmD3WjooCc153x",
-    //    "expirationTime": null,
-    //        "keys": {
-    //    "p256dh": "BJcR3HMUYiQoIOMJbIuvW7hO8ReprkOa_I1rjqOjj5Jta3UViPU5bixMzoaxlVxcpjFwXciqVgrBKMtuddMh2Lk",
-    //        "auth": "7gy4jP_uXxVFuANl-SREMw"
-    //}
 
 };
 
 // The actual payload to push
-webpush.sendNotification(pushSubscription, 'ST Push Info');
+// Check permissions granted  TODO
+webpush.sendNotification(pushSubscription, "ST Push Info");
 
